92c003fe7a85894c4d73bdc0ed98d5e2974fcff4
kirandeep123 <me@kiran.cc>
2019-07-24 03:55:17 +0530

    Updates

---
+++ precache-manifest.85b7076dc398f199bb5868ec526a3b7a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76554714893d5ddb81b1c8b4a5c9177e",
+    "url": "/MarleySpoon-Assignment/index.html"
+  },
+  {
+    "revision": "d5954cf30e84c61d27d6",
+    "url": "/MarleySpoon-Assignment/static/js/2.1f792f9e.chunk.js"
+  },
+  {
+    "revision": "9d80e1e1af20093b906b",
+    "url": "/MarleySpoon-Assignment/static/js/main.5075237a.chunk.js"
+  },
+  {
+    "revision": "1e769c7aa4d955b08fb6",
+    "url": "/MarleySpoon-Assignment/static/js/runtime~main.d718b49e.js"
+  }
+]);
